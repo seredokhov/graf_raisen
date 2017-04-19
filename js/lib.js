e0c5e3c7f4556703ad6938bc627a2167b21edb63
seredokhov <seredokhovp@mail.ru>
2017-04-20 00:45:54 +0200

    hot_deals

--- js/lib.js
+++ js/lib.js
@@ -3,3 +3,9 @@ $(function() {
 		$('.left_bar ul li').removeClass('active');
 	})
 });
+$(function() {
+    $("ul.sort li").click(function() {
+        $("ul.sort li").removeClass("active");
+        $(this).toggleClass("active");
+    })
+});
