fd13723eaf4fb225cbc2d33faaf6d248477ddf39
seredokhov <seredokhovp@mail.ru>
2017-04-20 21:37:54 +0200

    gallery fix+ hot_deals

--- js/lib.js
+++ js/lib.js
@@ -1,3 +1,9 @@
+/* Галерея */
+
+
+
+/*  ======*/
+
 $(function() {
 	$("#dom").click(function() {
 		$('.left_bar ul li').removeClass('active');
@@ -8,4 +14,35 @@ $(function() {
         $("ul.sort li").removeClass("active");
         $(this).toggleClass("active");
     })
+});
+var prev = document.querySelector('#gallery button.prev');
+var next = document.querySelector('#gallery button.next');
+var images = document.querySelectorAll('#gallery .photos img');
+var i = 0;
+
+next.onclick = function () {
+	images[i].style.opacity = '0';
+	i++;
+	if (i >= images.length) {
+		i = 0;
+	}
+	images[i].style.opacity = '1';
+}
+
+
+prev.onclick = function () {
+	images[i].style.opacity = '0';
+	i--;
+	if (i < 0) {
+		i = images.length - 1;
+	}
+	images[i].style.opacity = '1';
+}
+
+$(function() {
+	$("#gallery .small_galery div").click(function() {
+		i = $(this).attr('id');
+		$("#gallery .photos img").css("opacity","0");
+		images[i].style.opacity = '1';
+	})
 });
