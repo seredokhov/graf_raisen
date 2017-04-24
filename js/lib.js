286be9fdaed339e605294172be2d0973f9116f35
seredokhov <seredokhovp@mail.ru>
2017-04-24 13:57:56 +0200

    Human select JS

--- js/lib.js
+++ js/lib.js
@@ -78,6 +78,49 @@ $(function() {
 
 
 
+/* ====  Количество человек (выпадающее меню)  ====*/
+
+$(document).on(
+	'click.bs.dropdown.data-api', 
+	'[class*="fix"]',
+	function (e) { e.stopPropagation() }
+);
+
+$(function() {
+	$(".fix .range_inputs .applyBtn").click(function() {
+		$(this).parents('.open').removeClass("open");
+
+	})
+	$(".fix .range_inputs .cancelBtn").click(function() {
+		$('#human').val( 1);
+		$(".fix input[type='radio']").removeAttr("checked");
+		$(".fix input[name='man']:first").prop("checked", true);
+		$(this).parents('.open').removeClass("open");
+	})
+});
+
+
+/* Чекбоксы выпадающего меню  */
+
+$(function() {
+  var target = $('#human')
+    , men = $('.fix input[name="man"]')
+    , children = $('.fix input[name="child"]');
+  children.click(function() {
+    if ('on' === $(this).data('state')) {  
+      $(this).data('state', 'off').removeAttr('checked');
+    }
+    else {
+      children.data('state', 'off');
+      $(this).data('state', 'on').attr('checked', 'checked');
+    }
+  });
+  men.add(children).click(function() {
+    target.val((men.filter(':checked').val() || '')
+      + (children.filter(':checked').val() || ''));
+  });
+});
+
 /*   Календарь   */
 
 if (document.querySelector('input[name="daterange"]')) {
